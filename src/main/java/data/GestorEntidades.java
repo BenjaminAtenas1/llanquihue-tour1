9d3c89c23b5d19d168a34edf9eca84ef7a3afa16
BenjaminAtenas1 <ben.atenas@duocuc.cl>
2026-07-12 12:58:42 -0400

    Tarea Semana 8 POO

--- src/main/java/data/GestorEntidades.java
+++ src/main/java/data/GestorEntidades.java
@@ -23,21 +23,21 @@ public class GestorEntidades {
         List<Registrable> listaRegistrables = new ArrayList<>();
 
         //Dos objetos de la subclase RutaGastronomica
-        listaRegistrables.add(new RutaGastronomica("Ruta Peruana", 4, 3));
-        listaRegistrables.add(new RutaGastronomica("Ruta China", 4, 4));
+        //listaRegistrables.add(new RutaGastronomica("Ruta Peruana", 4, 3));
+        //listaRegistrables.add(new RutaGastronomica("Ruta China", 4, 4));
 
         //Dos objetos de la subclase Paseo Lacustre
-        listaRegistrables.add(new PaseoLacustre("Paseo en lancha", 2, "Lancha"));
-        listaRegistrables.add(new PaseoLacustre("Paseo en la moto acuática", 3, "Moto acuática"));
+        //listaRegistrables.add(new PaseoLacustre("Paseo en lancha", 2, "Lancha"));
+        //listaRegistrables.add(new PaseoLacustre("Paseo en la moto acuática", 3, "Moto acuática"));
 
         //Dos objetos de la subclase Excursión Cultural
-        listaRegistrables.add(new ExcursionCultural("Excursión de terror", 2, "Cementerio general"));
-        listaRegistrables.add(new ExcursionCultural("Excursión al persa", 3, "Persa Victor Manuel"));
+        //listaRegistrables.add(new ExcursionCultural("Excursión de terror", 2, "Cementerio general"));
+        //listaRegistrables.add(new ExcursionCultural("Excursión al persa", 3, "Persa Victor Manuel"));
 
         //3 Objetos que heredan de persona
-        listaRegistrables.add(new GuiaTuristico("Ramon Valdes", "16.252.135-5", "Puerto Montt", 3));
-        listaRegistrables.add(new Cliente("Florinda Meza", "65.134.816-5", 654321, "Ruta China"));
-        listaRegistrables.add(new Jefe("Roberto Gomez Bolaños", "52.457.643-5", "Metropolitana", 9));
+        //listaRegistrables.add(new GuiaTuristico("Ramon Valdes", "16.252.135-5", "Puerto Montt", 3));
+        //listaRegistrables.add(new Cliente("Florinda Meza", "65.134.816-5", 654321, "Ruta China"));
+        //listaRegistrables.add(new Jefe("Roberto Gomez Bolaños", "52.457.643-5", "Metropolitana", 9));
 
     }
 }
